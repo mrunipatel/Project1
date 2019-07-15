a176c40991a3dee7e075e50aa391ff01657d3783
Mrunmai <mrunipatil94@gmail.com>
2019-07-15 22:18:51 +0530

    First Commit

---
+++ src/main/java/JavaFirst/Project1/First.java
@@ -0,0 +1,14 @@
+package JavaFirst.Project1;
+
+public class First
+{
+	static int a=1,b=2;
+	static int c;
+
+	public static void main ( String args[] )
+	{
+		c=a+b;
+		System.out.println("Value is "+c);
+	}
+
+}
