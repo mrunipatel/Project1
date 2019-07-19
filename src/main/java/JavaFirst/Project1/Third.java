45e571a6e09f2ec8dd209b352229b02fdc1fb9b3
Mrunmai <mrunipatil94@gmail.com>
2019-07-19 20:56:20 +0530

    Third  java program

---
+++ src/main/java/JavaFirst/Project1/Third.java
@@ -0,0 +1,21 @@
+package JavaFirst.Project1;
+
+public class Third
+{
+
+	public static void Add (int x, int y)
+	{		
+		int z;
+		z=x+y;
+		display(z);
+	}
+	public static void display (int m)
+	{
+		System.out.println("Value == " +m);
+	}
+	public static void main(String args[])
+	{
+		int a=1,b=2;
+		Add(a,b);
+	}
+}
